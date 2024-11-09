26f2f0b6881abe15115df5304c0168c4f11d3571
maior <maior@DESKTOP-FHGLER9>
2024-11-09 16:06:19 +0200

    Добавьте файлы проекта.

---
+++ PLA/Program.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+    double x = 1.3;
+    double k = 4;
+    double a = Math.Log(Math.Abs(x));
+    double b = Math.Exp(2 * x) + a * x;
+    double y = x * Math.Pow(a, 3) + Math.Pow(b, 2);
+    Console.WriteLine("Значение y: " + y);
+}
+catch
+{
+    Console.WriteLine("Введите верное значение:");
+}
+        
